a7ad47b2b259da54a32406c2a6f50215caec2f02
Homorwunmi <omotayomowunmi@gmail.com>
2024-05-20 05:52:22 -0700

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size0):
         """Initialize a new Square.
 
         Args:
@@ -33,8 +33,8 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-            if self.__size == 0:
-                print("")
+        if self.__size == 0:
+            print("The square is empty.")
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
