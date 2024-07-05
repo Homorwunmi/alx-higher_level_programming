20132a372d0f2cdb4a4fd527c45da5f6d9645673
Homorwunmi <omotayomowunmi@gmail.com>
2024-07-05 09:19:33 -0700

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,15 +15,18 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    """Initialize an empty string and itirate over the text"""
+    formatted_text = ""
     s = 0
-    while s < len(text) and text[s] == ' ':
-        s += 1
-
     while s < len(text):
-        print(text[s], end="")
-        if text[s] == "\n" or text[s] in ".?:":
-            if text[s] in ".?:":
-                print("\n")
+        formatted_text += text[s]
+
+        """
+        Check if current character is one of the specified punctuation marks
+        """
+        if text[s] in ".?:":
+            formatted_text += "\n\n"
+            """Skip all following spaces"""
             s += 1
             while s < len(text) and text[s] == ' ':
                 s += 1
